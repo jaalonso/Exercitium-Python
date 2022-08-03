fa6500920f5676d50c63f85be322e3e18d9c7a98
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-03 17:56:42 +0200

    Simplifica test_media_aritmetica_de_tres_numeros

--- tests/test_media_aritmetica_de_tres_numeros.py
+++ tests/test_media_aritmetica_de_tres_numeros.py
@@ -2,9 +2,7 @@ from src.media_aritmetica_de_tres_numeros import media3a, media3b
 
 
 def test_media3():
-    assert media3a(1, 3, 8) == 4.0
-    assert media3a(-1, 0, 7) == 2.0
-    assert media3a(-3, 0, 3) == 0.0
-    assert media3b(1, 3, 8) == 4.0
-    assert media3b(-1, 0, 7) == 2.0
-    assert media3b(3, 0, -3) == 0.0
+    for media3 in [media3a, media3b]:
+        assert media3(1, 3, 8) == 4.0
+        assert media3(-1, 0, 7) == 2.0
+        assert media3(-3, 0, 3) == 0.0
