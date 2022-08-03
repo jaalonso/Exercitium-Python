a36cfda4fc9956b680c4f29ad295bcbddfd16cd6
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-03 19:08:02 +0200

    Inicio de suma_de_monedas.py

---
+++ src/suma_de_monedas.py
@@ -0,0 +1,21 @@
+# suma_de_monedas.py
+# Suma de monedas
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 9-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    sumaMonedas : (int, int, int, int, int) -> int
+# tal que sumaMonedas(a, b, c, d, e) es la suma de los euros
+# correspondientes a a monedas de 1 euro, b de 2 euros, c de 5 euros, d
+# 10 euros y e de 20 euros. Por ejemplo,
+#    sumaMonedas(0, 0, 0, 0, 1)  ==  20
+#    sumaMonedas(0, 0, 8, 0, 3)  == 100
+#    sumaMonedas(1, 1, 1, 1, 1)  ==  38
+# ---------------------------------------------------------------------
+
+
+def sumaMonedas(a, b, c, d, e):
+    # type: (int, int, int, int, int) -> int
+    return 1 * a + 2 * b + 5 * c + 10 * d + 20 * e
