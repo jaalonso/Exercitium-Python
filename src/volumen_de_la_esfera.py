63b5c4397b5fea1ba382a5f427e9260187ceba31
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-04 11:44:56 +0200

    Inicio de Volumen_de_la_esfera.py

---
+++ src/volumen_de_la_esfera.py
@@ -0,0 +1,20 @@
+# Volumen_de_la_esfera.py
+# Volumen de la esfera.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 10-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    volumenEsfera : (float) -> float
+# tal que volumenEsfera(r) es el volumen de la esfera de radio r. Por
+# ejemplo,
+#    volumenEsfera(10)  ==  4188.790204786391
+# ---------------------------------------------------------------------
+
+from math import pi
+
+
+def volumenEsfera(r):
+    # type: (float) -> float
+    return (4 / 3) * pi * r ** 3
