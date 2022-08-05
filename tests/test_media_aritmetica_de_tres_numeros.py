ad212a75c15d86446023e7cd2b18121192cc6e45
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-05 12:47:15 +0200

    Borrar 2ª solución en test_media_aritmetica_de_tres_numeros.py

--- tests/test_media_aritmetica_de_tres_numeros.py
+++ tests/test_media_aritmetica_de_tres_numeros.py
@@ -1,8 +1,7 @@
-from src.media_aritmetica_de_tres_numeros import media3a, media3b
+from src.media_aritmetica_de_tres_numeros import media3
 
 
 def test_media3():
-    for media3 in [media3a, media3b]:
-        assert media3(1, 3, 8) == 4.0
-        assert media3(-1, 0, 7) == 2.0
-        assert media3(-3, 0, 3) == 0.0
+    assert media3(1, 3, 8) == 4.0
+    assert media3(-1, 0, 7) == 2.0
+    assert media3(-3, 0, 3) == 0.0
