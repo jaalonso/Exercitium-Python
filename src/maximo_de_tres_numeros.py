6ca1379970f54d5e31b8ac5370aeb476577551e3
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-05 16:48:36 +0200

    Iniciar maximo_de_tres_numeros.py

---
+++ src/maximo_de_tres_numeros.py
@@ -0,0 +1,19 @@
+# maximo_de_tres_numeros.py
+# Máximo de tres números.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 15-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    maxTres: (int, int, int) -> int
+# tal que maxTres(x, y, z) es el máximo de x, y y z. Por ejemplo,
+#    maxTres(6, 2, 4)  ==  6
+#    maxTres(6, 7, 4)  ==  7
+#    maxTres(6, 7, 9)  ==  9
+# ---------------------------------------------------------------------
+
+
+def maxTres(x, y, z):
+    # type: (int, int, int) -> int
+    return max(x, max(y, z))
