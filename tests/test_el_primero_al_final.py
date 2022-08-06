b04f9cc5c85f32d08e227b8fbf6e1664ecb447af
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-06 07:35:10 +0200

    Añadir 2ª solución a test_el_primero_al_final.py

--- tests/test_el_primero_al_final.py
+++ tests/test_el_primero_al_final.py
@@ -1,6 +1,8 @@
-from src.el_primero_al_final import rota1
+from src.el_primero_al_final import rota1a, rota1b
 
 
 def test_rota1():
-    assert rota1([3, 2, 5, 7]) == [2, 5, 7, 3]
-    assert rota1(['a', 'b', 'c']) == ['b', 'c', 'a']
+    assert rota1a([3, 2, 5, 7]) == [2, 5, 7, 3]
+    assert rota1a(['a', 'b', 'c']) == ['b', 'c', 'a']
+    assert rota1b([3, 2, 5, 7]) == [2, 5, 7, 3]
+    assert rota1b(['a', 'b', 'c']) == ['b', 'c', 'a']
