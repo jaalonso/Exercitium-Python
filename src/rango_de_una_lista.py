d6d5494bdaef6f21bb691fa48d593f2a1be0de46
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-07 17:01:15 +0200

    Añadir enunciado a rango_de_una_lista.py.

---
+++ src/rango_de_una_lista.py
@@ -0,0 +1,13 @@
+# rango_de_una_lista.py
+# Rango de una lista.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 18-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    rango : (List[int]) -> List[int]
+# tal que rango(xs) es la lista formada por el menor y mayor elemento
+# de xs.
+#    rango([3, 2, 7, 5]) == [2, 7]
+# ---------------------------------------------------------------------
