d5c7ecb861f256712bcf33e4d7bb60b5d37ef00a
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-07 18:00:09 +0200

    Añadir enunciado a reconocimiento_de_palindromos.py.

---
+++ src/reconocimiento_de_palindromos.py
@@ -0,0 +1,15 @@
+# reconocimiento_de_palindromos.py
+# Reconocimiento de palíndromos.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 19-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    palindromo : (List[A]) -> bool
+# tal que palindromo(xs) se verifica si xs es un palíndromo; es decir,
+# es lo mismo leer xs de izquierda a derecha que de derecha a
+# izquierda. Por ejemplo,
+#    palindromo([3, 2, 5, 2, 3])     ==  True
+#    palindromo([3, 2, 5, 6, 2, 3])  ==  False
+# ---------------------------------------------------------------------
