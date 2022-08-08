025019b3a8577e2136feeea3c80221bcfbb69995
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-08 08:34:24 +0200

    Añadir enunciado a interior_de_una_lista.py.

---
+++ src/interior_de_una_lista.py
@@ -0,0 +1,13 @@
+# interior_de_una_lista.py
+# Interior de una lista
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 22-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    interior : (List[A]) -> List[A]
+# tal que interior(xs) es la lista obtenida eliminando los extremos de
+# la lista xs. Por ejemplo,
+#    interior([2, 5, 3, 7, 3])  ==  [5, 3, 7]
+# ---------------------------------------------------------------------
