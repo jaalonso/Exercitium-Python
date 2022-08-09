09760797d09cf778fd4fe0fe28fabc372183e924
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 17:23:18 +0200

    Añadir definición a primeros_y_ultimos_elementos.py.

---
+++ src/primeros_y_ultimos_elementos.py
@@ -0,0 +1,22 @@
+# Primeros_y_ultimos_elementos.py
+# Primeros y últimos elementos.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 25-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    extremos :: Int -> [a] -> [a]
+# tal que (extremos n xs) es la lista formada por los n primeros
+# elementos de xs y los n finales elementos de xs. Por ejemplo,
+#    extremos 3 [2,6,7,1,2,4,5,8,9,2,3]  ==  [2,6,7,9,2,3]
+# ---------------------------------------------------------------------
+
+from typing import List, TypeVar
+
+A = TypeVar('A')
+
+
+def extremos(n, xs):
+    # type: (int, List[A]) -> List[A]
+    return xs[:n] + xs[-n:]
