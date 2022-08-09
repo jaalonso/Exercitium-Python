6dbc173b53fafd9dfab8aea6d963fd9c0f61d6b2
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 17:26:22 +0200

    Añadir definición a primeros_y_ultimos_elementos.py.

--- src/primeros_y_ultimos_elementos.py
+++ src/primeros_y_ultimos_elementos.py
@@ -9,7 +9,7 @@
 #    extremos : (int, List[A]) -> List[A]
 # tal que extremos(n, xs) es la lista formada por los n primeros
 # elementos de xs y los n finales elementos de xs. Por ejemplo,
-#    extremos 3 [2, 6, 7, 1, 2, 4, 5, 8, 9, 2, 3]  ==  [2, 6, 7, 9, 2, 3]
+#    extremos(3, [2, 6, 7, 1, 2, 4, 5, 8, 9, 2, 3])  ==  [2, 6, 7, 9, 2, 3]
 # ---------------------------------------------------------------------
 
 from typing import List, TypeVar
