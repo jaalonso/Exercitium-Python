b89ccf67ecb3da17e953c28445905627ac6b75ed
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 13:57:30 +0200

    Añadir 2ª definición a test_segmento_de_una_lista.py.

--- tests/test_segmento_de_una_lista.py
+++ tests/test_segmento_de_una_lista.py
@@ -1,7 +1,10 @@
-from src.segmento_de_una_lista import segmento
+from src.segmento_de_una_lista import segmento1, segmento2
 
 
 def test_segmento():
-    assert segmento(3, 4, [3, 4, 1, 2, 7, 9, 0]) == [1, 2]
-    assert segmento(3, 5, [3, 4, 1, 2, 7, 9, 0]) == [1, 2, 7]
-    assert segmento(5, 3, [3, 4, 1, 2, 7, 9, 0]) == []
+    assert segmento1(3, 4, [3, 4, 1, 2, 7, 9, 0]) == [1, 2]
+    assert segmento1(3, 5, [3, 4, 1, 2, 7, 9, 0]) == [1, 2, 7]
+    assert segmento1(5, 3, [3, 4, 1, 2, 7, 9, 0]) == []
+    assert segmento2(3, 4, [3, 4, 1, 2, 7, 9, 0]) == [1, 2]
+    assert segmento2(3, 5, [3, 4, 1, 2, 7, 9, 0]) == [1, 2, 7]
+    assert segmento2(5, 3, [3, 4, 1, 2, 7, 9, 0]) == []
