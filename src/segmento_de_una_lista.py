56eec3c399e4f9473a748f56ec70c9d00feab598
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 13:53:43 +0200

    Corregir paréntesis en ejemplos de segmento_de_una_lista.py.

--- src/segmento_de_una_lista.py
+++ src/segmento_de_una_lista.py
@@ -9,9 +9,9 @@
 #    segmento : (int, int, List[A]) -> List[A]
 # tal que segmento(m, n, xs) es la lista de los elementos de xs
 # comprendidos entre las posiciones m y n. Por ejemplo,
-#    segmento(3, 4, [3, 4, 1, 2, 7, 9, 0]  ==  [1, 2]
-#    segmento(3, 5, [3, 4, 1, 2, 7, 9, 0]  ==  [1, 2, 7]
-#    segmento(5, 3, [3, 4, 1, 2, 7, 9, 0]  ==  []
+#    segmento(3, 4, [3, 4, 1, 2, 7, 9, 0])  ==  [1, 2]
+#    segmento(3, 5, [3, 4, 1, 2, 7, 9, 0])  ==  [1, 2, 7]
+#    segmento(5, 3, [3, 4, 1, 2, 7, 9, 0])  ==  []
 # ---------------------------------------------------------------------
 
 from typing import List, TypeVar
