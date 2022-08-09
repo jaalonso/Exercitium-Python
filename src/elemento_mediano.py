ecaf7e10eabed76f4aff1eff85207ed65cac0495
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 18:15:59 +0200

    Iniciar elemento_mediano.py.

---
+++ src/elemento_mediano.py
@@ -0,0 +1,21 @@
+# Elemento_mediano.py
+# Elemento mediano.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 26-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    mediano : (int, int, int) -> int
+# tal que mediano(x, y, z) es el número mediano de los tres números x, y
+# y z. Por ejemplo,
+#    mediano(3, 2, 5) == 3
+#    mediano(2, 4, 5) == 4
+#    mediano(2, 6, 5) == 5
+#    mediano(2, 6, 6) == 6
+# ---------------------------------------------------------------------
+
+
+def mediano(x, y, z):
+    # type: (int, int, int) -> int
+    return x + y + z - min([x, y, z]) - max([x, y, z])
