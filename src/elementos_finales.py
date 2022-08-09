fe0679fa70c1a0a708e5c0e6d93c6500fbafc0be
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 12:05:39 +0200

    Quitar tipos a elementos_finales.py.

--- src/elementos_finales.py
+++ src/elementos_finales.py
@@ -13,25 +13,17 @@
 # ---------------------------------------------------------------------
 
 
-from typing import List, TypeVar
-
-A = TypeVar('A')
-
-
 # 1ª definición
 def finales1(n, xs):
-    # type: (int, List[A]) -> List[A]
     return xs[len(xs) - n:]
 
 
 # 2ª definición
 def finales2(n, xs):
-    # type: (int, List[A]) -> List[A]
     return xs[-n:]
 
 
 # 3ª definición
 def finales3(n, xs):
-    # type: (int, List[A]) -> List[A]
     ys = list(reversed(xs))
     return list(reversed(ys[:n]))
