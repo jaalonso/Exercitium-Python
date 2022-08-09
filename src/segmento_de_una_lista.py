6edca388db026c99b6f3e985a4af32c13c1dde4f
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-09 13:48:37 +0200

    Iniciar segmento_de_una_lista.py.

---
+++ src/segmento_de_una_lista.py
@@ -0,0 +1,25 @@
+# segmento_de_una_lista.py
+# Segmento de una lista.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 24-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    segmento : (int, int, List[A]) -> List[A]
+# tal que segmento(m, n, xs) es la lista de los elementos de xs
+# comprendidos entre las posiciones m y n. Por ejemplo,
+#    segmento(3, 4, [3, 4, 1, 2, 7, 9, 0]  ==  [1, 2]
+#    segmento(3, 5, [3, 4, 1, 2, 7, 9, 0]  ==  [1, 2, 7]
+#    segmento(5, 3, [3, 4, 1, 2, 7, 9, 0]  ==  []
+# ---------------------------------------------------------------------
+
+from typing import List, TypeVar
+
+A = TypeVar('A')
+
+
+def segmento(m, n, xs):
+    # type: (int, int, List[A]) -> List[A]
+    ys = xs[:n]
+    return ys[m - 1:]
