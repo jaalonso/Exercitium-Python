2206699a856b8ea415078ef15f5fabd06997bd52
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-10 08:18:38 +0200

    Iniciar tres_iguales.py

---
+++ src/tres_iguales.py
@@ -0,0 +1,24 @@
+# Tres_iguales.py
+# Tres iguales
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 29-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    tresIguales : (int, int, int) -> bool
+# tal que tresIguales(x, y, z) se verifica si los elementos x, y y z son
+# iguales. Por ejemplo,
+#    tresIguales(4, 4, 4)  ==  True
+#    tresIguales(4, 3, 4)  ==  False
+# ---------------------------------------------------------------------
+
+# 1ª definición
+def tresIguales(x, y, z):
+    # type: (int, int, int) -> bool
+    return x == y & y == z
+
+
+# 2ª definición
+def tresIguales2(x, y, z):
+    return x == y == z
