3a2c50f73691dbfde123c18ba0d870b754824963
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-11 10:54:31 +0200

    Añadir definiciones a division_segura.py.

---
+++ src/division_segura.py
@@ -0,0 +1,29 @@
+# division_segura.py
+# División segura.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 31-agosto-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    divisionSegura :: Double -> Double -> Double
+# tal que (divisionSegura x y) es x/y si y no es cero y 9999 en caso
+# contrario. Por ejemplo,
+#    divisionSegura(7, 2)  ==  3.5
+#    divisionSegura(7, 0)  ==  9999.0
+# ---------------------------------------------------------------------
+
+# 1ª definición
+def divisionSegura1(x, y):
+    if y == 0:
+        return 9999.0
+    return x/float(y)
+
+
+# 2ª definición
+def divisionSegura2(x, y):
+    match y:
+        case 0:
+            return 9999.0
+        case _:
+            return x/float(y)
