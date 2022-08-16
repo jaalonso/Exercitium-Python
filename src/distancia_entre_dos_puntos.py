778cd284990894cffdb3128c4d86cac6eebd01ba
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-16 18:59:41 +0200

    Añadir propiedad triangular.

--- src/distancia_entre_dos_puntos.py
+++ src/distancia_entre_dos_puntos.py
@@ -10,9 +10,15 @@
 # tal que distancia(p1, p2) es la distancia entre los puntos p1 y
 # p2. Por ejemplo,
 #    distancia((1, 2), (4, 6)) == 5.0
+#
+# Comprobar con Hypothesis que se verifica la propiedad triangular de
+# la distancia; es decir, dados tres puntos p1, p2 y p3, la distancia
+# de p1 a p3 es menor o igual que la suma de la distancia de p1 a p2 y
+# la de p2 a p3.
 # ---------------------------------------------------------------------
 
 from math import sqrt
+from hypothesis import given, strategies as st
 
 
 def distancia(p1, p2):
@@ -20,3 +26,14 @@ def distancia(p1, p2):
     (x1, y1) = p1
     (x2, y2) = p2
     return sqrt((x1-x2)**2+(y1-y2)**2)
+
+
+# La propiedad es
+@given(st.tuples(st.integers(), st.integers()),
+       st.tuples(st.integers(), st.integers()),
+       st.tuples(st.integers(), st.integers()))
+def test_triangular(p1, p2, p3):
+    assert distancia(p1, p3) <= distancia(p1, p2) + distancia(p2, p3)
+
+# La comprobación es
+#    >>> test_triangular()
