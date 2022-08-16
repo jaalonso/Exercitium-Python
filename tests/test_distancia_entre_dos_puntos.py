d6052497aa0574d6f22ffc3a134e70e982f23ea2
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-16 18:50:42 +0200

    Añadir propiedad triangular con Hypothesis.

--- tests/test_distancia_entre_dos_puntos.py
+++ tests/test_distancia_entre_dos_puntos.py
@@ -1,5 +1,13 @@
+from hypothesis import given, strategies as st
 from src.distancia_entre_dos_puntos import distancia
 
 
 def test_distancia():
     assert distancia((1, 2), (4, 6)) == 5.0
+
+
+@given(st.tuples(st.integers(), st.integers()),
+       st.tuples(st.integers(), st.integers()),
+       st.tuples(st.integers(), st.integers()))
+def test_triangular(p1, p2, p3):
+    assert distancia(p1, p3) <= distancia(p1, p2) + distancia(p2, p3)
