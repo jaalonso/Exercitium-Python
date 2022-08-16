446167e886048860a50093e77ae22213ee588b14
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-16 19:37:55 +0200

    Añadir equivalencias a test_elementos_finales.py

--- tests/test_elementos_finales.py
+++ tests/test_elementos_finales.py
@@ -1,7 +1,11 @@
+from hypothesis import given, strategies as st
 from src.elementos_finales import finales1, finales2, finales3
-
 
 def test_finales():
     assert finales1(3, [2, 5, 4, 7, 9, 6]) == [7, 9, 6]
     assert finales2(3, [2, 5, 4, 7, 9, 6]) == [7, 9, 6]
     assert finales3(3, [2, 5, 4, 7, 9, 6]) == [7, 9, 6]
+
+@given(st.integers(), st.lists(st.integers()))
+def test_equiv_finales(n, xs):
+    assert finales1(n, xs) == finales2(n, xs) == finales3(n, xs)
