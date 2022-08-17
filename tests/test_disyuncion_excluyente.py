74276d031275c631705943d47f2ee9a7e2025a93
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 19:13:56 +0200

    Añadir equivalencia a test_disyuncion_excluyente.py

--- tests/test_disyuncion_excluyente.py
+++ tests/test_disyuncion_excluyente.py
@@ -1,5 +1,5 @@
+from hypothesis import given, strategies as st
 from src.disyuncion_excluyente import xor1, xor2, xor3, xor4, xor5
-
 
 def test_xor():
     for xor in [xor1, xor2, xor3, xor4, xor5]:
@@ -7,3 +7,7 @@ def test_xor():
         assert xor(True, False) is True
         assert xor(False, True) is True
         assert xor(False, False) is False
+
+@given(st.booleans(), st.booleans())
+def test_equiv_xor(x, y):
+    assert xor1(x, y) == xor2(x, y) == xor3(x, y) == xor4(x, y) == xor5(x, y)
