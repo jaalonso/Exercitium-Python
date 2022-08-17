66ac699c49417708034ff39a758138fcd55b02fa
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 11:31:12 +0200

    Añadir tipos y equivalencia a el_primero_al_final.py.

--- src/el_primero_al_final.py
+++ src/el_primero_al_final.py
@@ -13,18 +13,32 @@
 #    rota1(['a', 'b', 'c']) == ['b', 'c', 'a']
 # ---------------------------------------------------------------------
 
+from typing import List, TypeVar
+from hypothesis import given, strategies as st
+
+A = TypeVar('A')
+
 # 1ª solución
-# ===========
-
-
 def rota1a(xs):
+    # type: (List[A]) -> List[A]
+    if xs == []:
+        return []
     return xs[1:] + [xs[0]]
 
 # 2ª solución
-# ===========
-
-
 def rota1b(xs):
+    # type: (List[A]) -> List[A]
+    if xs == []:
+        return []
     ys = xs[1:]
     ys.append(xs[0])
     return ys
+
+# La equivalencia de las definiciones es
+@given(st.lists(st.integers()))
+def test_rota1(xs):
+    assert rota1a(xs) == rota1b(xs)
+
+# La comprobación es
+#    src> poetry run pytest -q el_primero_al_final.py
+#    1 passed in 0.20s
