b47c844ca7928573672318824990812a310b4e5c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 18:15:02 +0200

    Añadir equivalencia a test_tres_iguales.py

--- tests/test_tres_iguales.py
+++ tests/test_tres_iguales.py
@@ -1,8 +1,12 @@
+from hypothesis import given, strategies as st
 from src.tres_iguales import tresIguales1, tresIguales2
-
 
 def test_tresIguales():
     assert tresIguales1(4, 4, 4) is True
     assert tresIguales1(4, 3, 4) is False
     assert tresIguales2(4, 4, 4) is True
     assert tresIguales2(4, 3, 4) is False
+
+@given(st.integers(), st.integers(), st.integers())
+def test_equiv_tresIguales(x, y, z):
+    assert tresIguales1(x, y, z) == tresIguales2(x, y, z)
