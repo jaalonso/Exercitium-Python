e2202213c1f575bc17b2707aa9d9db09e21739ae
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 18:00:47 +0200

    Añadir equivalencia a test_segmento_de_una_lista.py

--- tests/test_segmento_de_una_lista.py
+++ tests/test_segmento_de_una_lista.py
@@ -1,5 +1,5 @@
+from hypothesis import given, strategies as st
 from src.segmento_de_una_lista import segmento1, segmento2
-
 
 def test_segmento():
     assert segmento1(3, 4, [3, 4, 1, 2, 7, 9, 0]) == [1, 2]
@@ -8,3 +8,7 @@ def test_segmento():
     assert segmento2(3, 4, [3, 4, 1, 2, 7, 9, 0]) == [1, 2]
     assert segmento2(3, 5, [3, 4, 1, 2, 7, 9, 0]) == [1, 2, 7]
     assert segmento2(5, 3, [3, 4, 1, 2, 7, 9, 0]) == []
+
+@given(st.integers(), st.integers(), st.lists(st.integers()))
+def test_equiv_segmento(m, n, xs):
+    assert segmento1(m, n, xs) == segmento2(m, n, xs)
