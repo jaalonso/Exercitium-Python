6c25e18504daa8f5366e632a497543aa3ad2af65
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 19:40:56 +0200

    Añadir propiedad a test_intercambio_de_componentes_de_un_par.py

--- tests/test_intercambio_de_componentes_de_un_par.py
+++ tests/test_intercambio_de_componentes_de_un_par.py
@@ -1,6 +1,10 @@
+from hypothesis import given, strategies as st
 from src.intercambio_de_componentes_de_un_par import intercambia
-
 
 def test_intercambia():
     assert intercambia((2, 5)) == (5, 2)
     assert intercambia((5, 2)) == (2, 5)
+
+@given(st.tuples(st.integers(), st.integers()))
+def test_equiv_intercambia(p):
+    assert intercambia(intercambia(p)) == p
