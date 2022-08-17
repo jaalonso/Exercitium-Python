7129fb49d6fb73a3a4ca1607bf84e52e28a3806c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 18:52:34 +0200

    Añadir equivalencia a test_division_segura.py

--- tests/test_division_segura.py
+++ tests/test_division_segura.py
@@ -1,8 +1,13 @@
+from hypothesis import given, strategies as st
 from src.division_segura import divisionSegura1, divisionSegura2
-
 
 def test_divisionSegura():
     assert divisionSegura1(7, 2) == 3.5
     assert divisionSegura1(7, 0) == 9999.0
     assert divisionSegura2(7, 2) == 3.5
     assert divisionSegura2(7, 0) == 9999.0
+
+@given(st.floats(allow_nan=False, allow_infinity=False),
+       st.floats(allow_nan=False, allow_infinity=False))
+def test_equiv_divisionSegura(x, y):
+    assert divisionSegura1(x, y) == divisionSegura2(x, y)
