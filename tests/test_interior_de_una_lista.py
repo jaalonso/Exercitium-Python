939b238e2d9d263644f5636ee574d4f2ec822a1b
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 17:12:25 +0200

    Añadir tipos y equivalencia a test_interior_de_una_lista.py.

--- tests/test_interior_de_una_lista.py
+++ tests/test_interior_de_una_lista.py
@@ -1,6 +1,10 @@
+from hypothesis import given, strategies as st
 from src.interior_de_una_lista import interior1, interior2
-
 
 def test_interior():
     assert interior1([2, 5, 3, 7, 3]) == [5, 3, 7]
     assert interior2([2, 5, 3, 7, 3]) == [5, 3, 7]
+
+@given(st.lists(st.integers()))
+def test_triangular(xs):
+    assert interior1(xs) == interior2(xs)
