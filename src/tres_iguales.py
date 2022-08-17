1da7f3a65da5ef4d7fff6a2ac6959d48fd0a4eda
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-17 18:12:51 +0200

    Añadir tipos y equivalencia a tres_iguales.py

--- src/tres_iguales.py
+++ src/tres_iguales.py
@@ -13,11 +13,23 @@
 #    tresIguales(4, 3, 4)  ==  False
 # ---------------------------------------------------------------------
 
+from hypothesis import given, strategies as st
+
 # 1ª definición
 def tresIguales1(x, y, z):
+    # type: (int, int, int) -> bool
     return x == y and y == z
-
 
 # 2ª definición
 def tresIguales2(x, y, z):
+    # type: (int, int, int) -> bool
     return x == y == z
+
+# La propiedad de equivalencia es
+@given(st.integers(), st.integers(), st.integers())
+def test_equiv_finales(x, y, z):
+    assert tresIguales1(x, y, z) == tresIguales2(x, y, z)
+
+# La comprobación es
+#    src> poetry run pytest -q tres_iguales.py
+#    1 passed in 0.16s
