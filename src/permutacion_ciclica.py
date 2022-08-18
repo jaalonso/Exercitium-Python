6b566d8a1ff6ea67fc33198575adabbd906c80cb
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-18 08:58:10 +0200

    Iniciar permutacion_ciclica.py

---
+++ src/permutacion_ciclica.py
@@ -0,0 +1,39 @@
+# permutacion_ciclica.py
+# Permutación cíclica
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 7-septiembre-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir una función
+#    ciclo : (List[A]) -> List[A]
+# tal que ciclo(xs) es la lista obtenida permutando cíclicamente los
+# elementos de la lista xs, pasando el último elemento al principio de
+# la lista. Por ejemplo,
+#    ciclo([2, 5, 7, 9])  == [9, 2, 5, 7]
+#    ciclo([])            == []
+#    ciclo([2])           == [2]
+#
+# Comprobar que la longitud es un invariante de la función ciclo; es
+# decir, la longitud de (ciclo xs) es la misma que la de xs.
+# ---------------------------------------------------------------------
+
+from typing import TypeVar, List
+from hypothesis import given, strategies as st
+
+A = TypeVar('A')
+
+def ciclo(xs):
+    # type: (List[A]) -> List[A]
+    if xs:
+        return [xs[-1]] + xs[:-1]
+    return []
+
+# La propiedad de es
+@given(st.lists(st.integers()))
+def test_equiv_ciclo(xs):
+    assert len(ciclo(xs)) == len(xs)
+
+# La comprobación es
+#    src> poetry run pytest -q permutacion_ciclica.py
+#    1 passed in 0.39s
