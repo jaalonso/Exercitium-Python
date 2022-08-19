2a28209677d3cb030da325705f165b56eac0f1a8
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-19 08:57:14 +0200

    Iniciar mayor_numero_con_dos_digitos_dados.py

--- src/mayor_numero_con_dos_digitos_dados.py
+++ src/mayor_numero_con_dos_digitos_dados.py
@@ -6,25 +6,28 @@
 
 # ---------------------------------------------------------------------
 # Definir la función
-#    numeroMayor :: Int -> Int -> Int
-# tal que (numeroMayor x y) es el mayor número de dos cifras que puede
+#    numeroMayor : (int, int) -> int
+# tal que numeroMayor(x, y) es el mayor número de dos cifras que puede
 # construirse con los dígitos x e y. Por ejemplo,
-#    numeroMayor 2 5 ==  52
-#    numeroMayor 5 2 ==  52
+#    numeroMayor(2, 5) == 52
+#    numeroMayor(5, 2) == 52
 # ---------------------------------------------------------------------
 
 # 1ª definición
 def numeroMayor1(x, y):
+    # type: (int, int) -> int
     return 10 * max(x, y) + min(x, y)
 
 # 2ª definición
 def numeroMayor2(x, y):
+    # type: (int, int) -> int
     if x > y:
-        return 10*x+y
-    return 10*y+x
+        return 10 * x + y
+    return 10 * y + x
 
 # La propiedad de equivalencia de las definiciones es
 def test_equiv_numeroMayor():
+    # type: () -> bool
     return all(numeroMayor1(x, y) == numeroMayor2(x, y)
                for x in range(10) for y in range(10))
 
