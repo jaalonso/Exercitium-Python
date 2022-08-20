35f4d7f3d21c17288f582e0b88caf4a4aabd8bc4
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-20 16:46:46 +0200

    Cambiar tipo en mayor_rectangulo.py

--- src/mayor_rectangulo.py
+++ src/mayor_rectangulo.py
@@ -9,8 +9,8 @@
 # ejemplo, (5,3) representa a un rectángulo de base 5 y altura 3.
 #
 # Definir la función
-#    mayorRectangulo : (Tuple[float, float], Tuple[float, float])
-#                      -> Tuple[float, float]
+#    mayorRectangulo : (tuple[float, float], tuple[float, float])
+#                      -> tuple[float, float]
 # tal que mayorRectangulo(r1, r2) es el rectángulo de mayor área entre
 # r1 y r2. Por ejemplo,
 #    mayorRectangulo((4, 6), (3, 7))  ==  (4, 6)
@@ -18,10 +18,8 @@
 #    mayorRectangulo((4, 6), (3, 9))  ==  (3, 9)
 # ---------------------------------------------------------------------
 
-from typing import Tuple
-
-def mayorRectangulo(r1, r2):
-    # type: (Tuple[float, float], Tuple[float, float]) -> Tuple[float, float]
+def mayorRectangulo(r1: tuple[float, float],
+                    r2: tuple[float, float]) -> tuple[float, float]:
     (a, b) = r1
     (c, d) = r2
     if a*b >= c*d:
