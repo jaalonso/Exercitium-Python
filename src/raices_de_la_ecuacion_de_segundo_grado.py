d49c57e77043bf908215ed8b57b26cb5a9055c53
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-21 09:16:40 +0200

    Iniciar raices_de_la_ecuacion_de_segundo_grado.py

---
+++ src/raices_de_la_ecuacion_de_segundo_grado.py
@@ -0,0 +1,55 @@
+# raices_de_la_ecuacion_de_segundo_grado.py
+# Raíces de la ecuación de segundo grado.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 12-septiembre-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    raices : (float, float, float) -> list[float]
+# tal que raices(a, b, c) es la lista de las raíces reales de la
+# ecuación ax^2 + bx + c = 0. Por ejemplo,
+#    raices(1, 3, 2)    ==  [-1.0,-2.0]
+#    raices(1, (-2), 1) ==  [1.0,1.0]
+#    raices(1, 0, 1)    ==  []
+#
+# Comprobar con Hypothesis que la suma de las raíces de la ecuación
+# ax^2 + bx + c = 0 (con a no nulo) es -b/a y su  producto es c/a.
+# ---------------------------------------------------------------------
+
+from math import sqrt
+from hypothesis import given, assume, strategies as st
+
+def raices(a: float, b: float, c: float) -> list[float]:
+    d = b**2 - 4*a*c
+    if d >= 0:
+        e = sqrt(d)
+        t = 2*a
+        return [(-b+e)/t, (-b-e)/t]
+    return []
+
+# Para comprobar la propiedad se usará la función
+#    casiIguales : (float, float) -> bool
+# tal que casiIguales(x, y) se verifica si x e y son casi iguales; es
+# decir si el valor absoluto de su diferencia es menor que una
+# milésima. Por  ejemplo,
+#    casiIguales(12.3457, 12.3459)  ==  True
+#    casiIguales(12.3457, 12.3479)  ==  False
+def casiIguales(x: float, y: float) -> bool:
+    return abs(x - y) < 0.001
+
+# La propiedad es
+@given(st.floats(min_value=-100, max_value=100),
+       st.floats(min_value=-100, max_value=100),
+       st.floats(min_value=-100, max_value=100))
+def test_prop_raices(a, b, c):
+    assume(abs(a) > 0.1)
+    xs = raices(a, b, c)
+    assume(xs)
+    [x1, x2] = xs
+    assert casiIguales(x1 + x2, -b / a)
+    assert casiIguales(x1 * x2, c / a)
+
+# La comprobación es
+#    src> poetry run pytest -q raices_de_la_ecuacion_de_segundo_grado.py
+#    1 passed in 0.35s
