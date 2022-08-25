a26fe16aaaca57767df3c532bc975d8556b7abdc
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-25 12:58:27 +0200

    Iniciar test_reconocimiento_de_subconjunto.py

---
+++ tests/test_reconocimiento_de_subconjunto.py
@@ -0,0 +1,12 @@
+from src.reconocimiento_de_subconjunto\
+ import subconjunto1, subconjunto2, subconjunto3, subconjunto4
+
+def test_subconjunto():
+    assert subconjunto1([3, 2, 3], [2, 5, 3, 5]) is True
+    assert subconjunto1([3, 2, 3], [2, 5, 6, 5]) is False
+    assert subconjunto2([3, 2, 3], [2, 5, 3, 5]) is True
+    assert subconjunto2([3, 2, 3], [2, 5, 6, 5]) is False
+    assert subconjunto3([3, 2, 3], [2, 5, 3, 5]) is True
+    assert subconjunto3([3, 2, 3], [2, 5, 6, 5]) is False
+    assert subconjunto4([3, 2, 3], [2, 5, 3, 5]) is True
+    assert subconjunto4([3, 2, 3], [2, 5, 6, 5]) is False
