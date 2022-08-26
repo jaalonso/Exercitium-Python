cacfadb20277e8ac34a39b10ea6539e834b6383c
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-26 12:38:44 +0200

    Iniciar test_igualdad_de_conjuntos.py

---
+++ tests/test_igualdad_de_conjuntos.py
@@ -0,0 +1,11 @@
+from src.igualdad_de_conjuntos import iguales1, iguales2
+
+def test_iguales():
+    assert iguales1([3, 2, 3], [2, 3])
+    assert iguales1([3, 2, 3], [2, 3, 2])
+    assert not iguales1([3, 2, 3], [2, 3, 4])
+    assert not iguales1([2, 3], [4, 5])
+    assert iguales2([3, 2, 3], [2, 3])
+    assert iguales2([3, 2, 3], [2, 3, 2])
+    assert not iguales2([3, 2, 3], [2, 3, 4])
+    assert not iguales2([2, 3], [4, 5])
