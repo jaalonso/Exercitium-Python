76f8a02191b5e65ed6f2c13eb50cc4e95de6dcb7
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-08-29 12:51:02 +0200

    Iniciar test_union_conjuntista_de_listas.py

---
+++ tests/test_union_conjuntista_de_listas.py
@@ -0,0 +1,7 @@
+from src.union_conjuntista_de_listas import union1, union2, union3, union4
+
+def test_union():
+    assert sorted(union1([3, 2, 5], [5, 7, 3, 4])) == [2, 3, 4, 5, 7]
+    assert sorted(union2([3, 2, 5], [5, 7, 3, 4])) == [2, 3, 4, 5, 7]
+    assert sorted(union3([3, 2, 5], [5, 7, 3, 4])) == [2, 3, 4, 5, 7]
+    assert sorted(union4([3, 2, 5], [5, 7, 3, 4])) == [2, 3, 4, 5, 7]
