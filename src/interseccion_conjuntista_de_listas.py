dbb4e57cefbfbc9b1212a6a1f3d6cd5a0a7b3641
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-02 13:58:10 +0200

    Iniciar interseccion_conjuntista_de_listas.py

---
+++ src/interseccion_conjuntista_de_listas.py
@@ -0,0 +1,91 @@
+# interseccion_conjuntista_de_listas.py
+# Intersección conjuntista de listas.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 21-septiembre-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Definir la función
+#    interseccion :: Eq a => [a] -> [a] -> [a]
+# tal que (interseccion xs ys) es la intersección de las listas sin
+# elementos repetidos xs e ys. Por ejemplo,
+#    interseccion [3,2,5] [5,7,3,4]  ==  [3,5]
+#    interseccion [3,2,5] [9,7,6,4]  ==  []
+# ---------------------------------------------------------------------
+
+from typing import TypeVar
+from timeit import Timer, default_timer
+from sys import setrecursionlimit
+from hypothesis import given, strategies as st
+
+setrecursionlimit(10**6)
+
+A = TypeVar('A')
+
+# 1ª solución
+# ===========
+
+def interseccion1(xs: list[A], ys: list[A]) -> list[A]:
+    return [x for x in xs if x in ys]
+
+# 2ª solución
+# ===========
+
+def interseccion2(xs: list[A], ys: list[A]) -> list[A]:
+    if not xs:
+        return []
+    if xs[0] in ys:
+        return [xs[0]] + interseccion2(xs[1:], ys)
+    return interseccion2(xs[1:], ys)
+
+# 3ª solución
+# ===========
+
+def interseccion3(xs: list[A], ys: list[A]) -> list[A]:
+    zs = []
+    for x in xs:
+        if x in ys:
+            zs.append(x)
+    return zs
+
+# 4ª solución
+# ===========
+
+def interseccion4(xs: list[A], ys: list[A]) -> list[A]:
+    return list(set(xs) & set(ys))
+
+# Comprobación de equivalencia
+# ============================
+#
+# La propiedad es
+@given(st.lists(st.integers()),
+       st.lists(st.integers()))
+def test_interseccion(xs, ys):
+    xs1 = list(set(xs))
+    ys1 = list(set(ys))
+    assert sorted(interseccion1(xs1, ys1)) ==\
+           sorted(interseccion2(xs1, ys1)) ==\
+           sorted(interseccion3(xs1, ys1)) ==\
+           sorted(interseccion4(xs1, ys1))
+
+# La comprobación es
+#    src> poetry run pytest -q interseccion_conjuntista_de_listas.py
+#    1 passed in 0.33s
+
+# Comparación de eficiencia
+# =========================
+
+def tiempo(e):
+    """Tiempo (en segundos) de evaluar la expresión e."""
+    t = Timer(e, "", default_timer, globals()).timeit(1)
+    print(f"{t:0.2f} segundos")
+
+# La comparación es
+#    >>> tiempo('interseccion1(list(range(0,20000)), list(range(1,20000,2)))')
+#    0.98 segundos
+#    >>> tiempo('interseccion2(list(range(0,20000)), list(range(1,20000,2)))')
+#    2.13 segundos
+#    >>> tiempo('interseccion3(list(range(0,20000)), list(range(1,20000,2)))')
+#    0.87 segundos
+#    >>> tiempo('interseccion4(list(range(0,20000)), list(range(1,20000,2)))')
+#    0.00 segundos
