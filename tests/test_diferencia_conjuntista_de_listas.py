5c4b18c5942ab4d01a00cc5bc3a19bdcfe72949a
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-04 11:39:09 +0200

    Iniciar test_diferencia_conjuntista_de_listas.py

---
+++ tests/test_diferencia_conjuntista_de_listas.py
@@ -0,0 +1,12 @@
+from src.diferencia_conjuntista_de_listas\
+    import diferencia1, diferencia2, diferencia3, diferencia4
+
+def test_diferencia():
+    assert diferencia1([3, 2, 5, 6], [5, 7, 3, 4]) == [2, 6]
+    assert diferencia1([3, 2, 5], [5, 7, 3, 2])    == []
+    assert diferencia2([3, 2, 5, 6], [5, 7, 3, 4]) == [2, 6]
+    assert diferencia2([3, 2, 5], [5, 7, 3, 2])    == []
+    assert diferencia3([3, 2, 5, 6], [5, 7, 3, 4]) == [2, 6]
+    assert diferencia3([3, 2, 5], [5, 7, 3, 2])    == []
+    assert diferencia4([3, 2, 5, 6], [5, 7, 3, 4]) == [2, 6]
+    assert diferencia4([3, 2, 5], [5, 7, 3, 2])    == []
