b3cd120e2bab038dc8ad88172b1192e3bf09080d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-08 11:51:30 +0200

    Iniciar test_suma_de_los_primeros_numeros_naturales.py

---
+++ tests/test_suma_de_los_primeros_numeros_naturales.py
@@ -0,0 +1,15 @@
+from src.suma_de_los_primeros_numeros_naturales import \
+    suma1, \
+    suma2, \
+    suma3, \
+    suma4, \
+    suma5, \
+    suma6
+
+def test_suma_de_los_primeros_numeros_naturales():
+    assert suma1(3) == 6
+    assert suma2(3) == 6
+    assert suma3(3) == 6
+    assert suma4(3) == 6
+    assert suma5(3) == 6
+    assert suma6(3) == 6
