e6d9464a764227ba47881d8b79d2a7f8e54759d6
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-09 18:41:02 +0200

    Iniciar tests/test_suma_de_cuadrados_menos_cuadrado_de_la_suma.py

---
+++ tests/test_suma_de_cuadrados_menos_cuadrado_de_la_suma.py
@@ -0,0 +1,15 @@
+from src.suma_de_cuadrados_menos_cuadrado_de_la_suma import \
+    euler6a, \
+    euler6b, \
+    euler6c, \
+    euler6d, \
+    euler6e, \
+    euler6f
+
+def test_euler6():
+    assert euler6a(10) == 2640
+    assert euler6b(10) == 2640
+    assert euler6c(10) == 2640
+    assert euler6d(10) == 2640
+    assert euler6e(10) == 2640
+    assert euler6f(10) == 2640
