72aabb0096c625d19048f09680e191dc9077bc45
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-23 12:53:46 +0200

    Reescribir src/numeros_perfectos.py

--- src/numeros_perfectos.py
+++ src/numeros_perfectos.py
@@ -18,12 +18,11 @@
 #    perfectos(10^5)  ==  [6, 28, 496, 8128]
 # ---------------------------------------------------------------------
 
-from math import sqrt
 from timeit import Timer, default_timer
 
 from hypothesis import given
 from hypothesis import strategies as st
-from sympy import divisors
+from sympy import divisor_sigma
 
 # 1ª solución
 # ===========
@@ -33,34 +32,38 @@ from sympy import divisors
 def divisores1(n: int) -> list[int]:
     return [x for x in range(1, n + 1) if n % x == 0]
 
+# sumaDivisores(x) es la suma de los divisores de x. Por ejemplo,
+#    sumaDivisores(12)                ==  28
+#    sumaDivisores(25)                ==  31
+def sumaDivisores1(n: int) -> int:
+    return sum(divisores1(n))
+
+# esPerfecto(x) se verifica si x es un número perfecto. Por ejemplo,
+#    esPerfecto(6)  ==  True
+#    esPerfecto(8)  ==  False
+def esPerfecto1(x: int) -> bool:
+    return sumaDivisores1(x) - x == x
+
 def perfectos1(n: int) -> list[int]:
-    return [x for x in range(1, n + 1) if sum(divisores1(x)) == 2 * x]
+    return [x for x in range(1, n + 1) if esPerfecto1(x)]
 
 # 2ª solución
 # ===========
 
-# primerosDivisores(n) es la lista de los divisores del número n cuyo
-# cuadrado es menor o gual que n. Por ejemplo,
-#    primerosDivisores(25)  ==  [1,5]
-#    primerosDivisores(30)  ==  [1,2,3,5]
-def primerosDivisores2(n: int) -> list[int]:
-    return [x for x in range(1, 1 + round(sqrt(n))) if n % x == 0]
+# Sustituyendo la definición de sumaDivisores de la solución anterior por
+# cada una de las del ejercicio [Suma de divisores](https://bit.ly/3S9aonQ)
+# se obtiene una nueva definición deperfectos. La usada en la
+# definición anterior es la menos eficiente y la que se usa en la
+# siguiente definición es la más eficiente.
 
-def divisores2(n: int) -> list[int]:
-    xs = primerosDivisores2(n)
-    zs = list(reversed(xs))
-    if zs[0]**2 == n:
-        return xs + [n // a for a in zs[1:]]
-    return xs + [n // a for a in zs]
+def sumaDivisores2(n: int) -> int:
+    return divisor_sigma(n, 1)
+
+def esPerfecto2(x: int) -> bool:
+    return sumaDivisores2(x) - x == x
 
 def perfectos2(n: int) -> list[int]:
-    return [x for x in range(1, n + 1) if sum(divisores2(x)) == 2 * x]
-
-# 3ª solución
-# ===========
-
-def perfectos3(n: int) -> list[int]:
-    return [x for x in range(1, n + 1) if sum(divisors(x)) == 2 * x]
+    return [x for x in range(1, n + 1) if esPerfecto2(x)]
 
 # Comprobación de equivalencia
 # ============================
@@ -68,9 +71,7 @@ def perfectos3(n: int) -> list[int]:
 # La propiedad es
 @given(st.integers(min_value=2, max_value=1000))
 def test_perfectos(n):
-    assert perfectos1(n) ==\
-           perfectos2(n) ==\
-           perfectos3(n)
+    assert perfectos1(n) == perfectos2(n)
 
 # La comprobación es
 #    src> poetry run pytest -q numeros_perfectos.py
@@ -86,13 +87,6 @@ def tiempo(e):
 
 # La comparación es
 #    >>> tiempo('perfectos1(10**4)')
-#    2.27 segundos
+#    2.97 segundos
 #    >>> tiempo('perfectos2(10**4)')
-#    0.07 segundos
-#    >>> tiempo('perfectos3(10**4)')
-#    0.11 segundos
-#
-#    >>> tiempo('perfectos2(3 * 10**5)')
-#    4.65 segundos
-#    >>> tiempo('perfectos3(3 * 10**5)')
-#    3.52 segundos
+#    0.57 segundos
