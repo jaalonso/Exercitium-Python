00725cc319841fa58459ec307ccc344dc0b30ea4
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-23 12:55:22 +0200

    Reescribir tests/test_numeros_perfectos.py

--- tests/test_numeros_perfectos.py
+++ tests/test_numeros_perfectos.py
@@ -1,9 +1,7 @@
 from src.numeros_perfectos import \
     perfectos1, \
-    perfectos2, \
-    perfectos3
+    perfectos2
 
 def test_perfectos():
     assert perfectos1(500) == [6, 28, 496]
     assert perfectos2(500) == [6, 28, 496]
-    assert perfectos3(500) == [6, 28, 496]
