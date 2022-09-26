f66bf2fe9470d380f0e255c4c530850e94eec086
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-26 12:38:13 +0200

    Iniciar tests/test_numeros_abundantes_impares.py

---
+++ tests/test_numeros_abundantes_impares.py
@@ -0,0 +1,7 @@
+from src.numeros_abundantes_impares import \
+    abundantesImpares1, abundantesImpares2, abundantesImpares3
+
+def test_abundantesImpares() -> None:
+    assert abundantesImpares1(1000)[0] == 945
+    assert abundantesImpares2(1000)[0] == 945
+    assert abundantesImpares3(1000)[0] == 945
