028beb264917dcc80e65509b5ab2bf14c9ccd76b
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-29 11:20:19 +0200

    Ampliar tests/test_puntos_dentro_del_circulo.py

--- tests/test_puntos_dentro_del_circulo.py
+++ tests/test_puntos_dentro_del_circulo.py
@@ -1,8 +1,10 @@
-from src.puntos_dentro_del_circulo import circulo1, circulo2
+from src.puntos_dentro_del_circulo import \
+    circulo1, circulo2, circulo3, circulo4
 
 def test_circulo() -> None:
-    for circulo in [circulo1, circulo2]:
-        assert circulo(3) == 9
-        assert circulo(4) == 15
-        assert circulo(5) == 22
-        assert circulo(100) == 7949
+    for circulo in [circulo1, circulo2, circulo3, circulo4]:
+        assert circulo(1)    ==  3
+        assert circulo(2)    ==  6
+        assert circulo(3)    ==  11
+        assert circulo(4)    ==  17
+        assert circulo(100)  ==  7955
