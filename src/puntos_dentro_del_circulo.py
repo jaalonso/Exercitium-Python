a875bc20648dacc53bc2fa1798c977afe115b489
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-09-29 11:17:12 +0200

    Ampliar src/puntos_dentro_del_circulo.py

--- src/puntos_dentro_del_circulo.py
+++ src/puntos_dentro_del_circulo.py
@@ -8,14 +8,15 @@
 # Definir la función
 #    circulo : (int) -> int
 # tal que circulo(n) es el la cantidad de pares de números naturales
-# (x,y) que se encuentran dentro del círculo de radio n. Por ejemplo,
-#    circulo(3)  ==  9
-#    circulo(4)  ==  15
-#    circulo(5)  ==  22
-#    circulo(100)  ==  7949
+# (x,y) que se encuentran en el círculo de radio n. Por ejemplo,
+#    circulo(1)    ==  3
+#    circulo(2)    ==  6
+#    circulo(3)    ==  11
+#    circulo(4)    ==  17
+#    circulo(100)  ==  7955
 # ---------------------------------------------------------------------
 
-from math import sqrt
+from math import sqrt, trunc, ceil
 from timeit import Timer, default_timer
 
 from hypothesis import given
@@ -25,23 +26,45 @@ from hypothesis import strategies as st
 # ===========
 
 def circulo1(n: int) -> int:
-    return len([(x, y) for x in range(0, n) for y in range(0, n)
-                if x * x + y * y < n * n])
+    return len([(x, y)
+                for x in range(0, n + 1)
+                for y in range(0, n + 1)
+                if x * x + y * y <= n * n])
 
 # 2ª solución
 # ===========
 
-# raizCuadradaEntera(n) es la parte entera de la raíz cuadrada de
-# n. Por ejemplo,
-#    raizCuadradaEntera 17  ==  4
-def raizCuadradaEntera(n: int) -> int:
-    return int(sqrt(n))
+def enSemiCirculo(n: int) -> list[tuple[int, int]]:
+    return [(x, y)
+            for x in range(0, ceil(sqrt(n**2)) + 1)
+            for y in range(x+1, trunc(sqrt(n**2 - x**2)) + 1)]
 
 def circulo2(n: int) -> int:
-    return len([(x, y)
-                for x in range(0, n)
-                for y in range(0, raizCuadradaEntera(n * n - x * x) + 1)
-                if x * x + y * y < n * n])
+    if n == 0:
+        return 1
+    return (2 * len(enSemiCirculo(n)) + ceil(n / sqrt(2)))
+
+# 3ª solución
+# ===========
+
+def circulo3(n: int) -> int:
+    r = 0
+    for x in range(0, n + 1):
+        for y in range(0, n + 1):
+            if x**2 + y**2 <= n**2:
+                r = r + 1
+    return r
+
+# 4ª solución
+# ===========
+
+def circulo4(n: int) -> int:
+    r = 0
+    for x in range(0, ceil(sqrt(n**2)) + 1):
+        for y in range(x + 1, trunc(sqrt(n**2 - x**2)) + 1):
+            if x**2 + y**2 <= n**2:
+                r = r + 1
+    return 2 * r + ceil(n / sqrt(2))
 
 # Comprobación de equivalencia
 # ============================
@@ -49,11 +72,14 @@ def circulo2(n: int) -> int:
 # La propiedad es
 @given(st.integers(min_value=1, max_value=100))
 def test_circulo(n: int) -> None:
-    assert circulo1(n) == circulo2(n)
+    r = circulo1(n)
+    assert circulo2(n) == r
+    assert circulo3(n) == r
+    assert circulo4(n) == r
 
 # La comprobación es
 #    src> poetry run pytest -q puntos_dentro_del_circulo.py
-#    1 passed in 0.34s
+#    1 passed in 0.60s
 
 # Comparación de eficiencia
 # =========================
@@ -68,3 +94,7 @@ def tiempo(e: str) -> None:
 #    0.71 segundos
 #    >>> tiempo('circulo2(2000)')
 #    0.76 segundos
+#    >>> tiempo('circulo3(2000)')
+#    2.63 segundos
+#    >>> tiempo('circulo4(2000)')
+#    1.06 segundos
