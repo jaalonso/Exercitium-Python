70cee67a015477c041913eebfe8e7f6866f1a068
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-12-06 08:36:57 +0100

    altura_de_un_arbol_binario

---
+++ src/altura_de_un_arbol_binario.py
@@ -0,0 +1,69 @@
+# altura_de_un_arbol_binario.py
+# Altura de un árbol binario.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 6-diciembre-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# El árbol binario
+#         ·
+#        / \
+#       /   \
+#      ·     ·
+#     / \   / \
+#    1   4 6   9
+# se puede representar por
+#    ejArbol = Nodo(Nodo(Hoja(1), Hoja(4)),
+#                   Nodo(Hoja(6), Hoja(9)))
+#
+#
+# El tipo de los árboles binarios se puede definir por
+#    @dataclass
+#    class Arbol:
+#        pass
+#
+#    @dataclass
+#    class Hoja(Arbol):
+#        x: int
+#
+#    @dataclass
+#    class Nodo(Arbol):
+#        i: Arbol
+#        d: Arbol
+#
+# Definir la función
+#    altura : (Arbol) -> int
+# tal que altura(t) es la altura del árbol t. Por ejemplo,
+#    >>> altura(Hoja(1))
+#    0
+#    >>> altura(Nodo(Hoja(1), Hoja(6)))
+#    1
+#    >>> altura(Nodo(Nodo(Hoja(1), Hoja(6)), Hoja(2)))
+#    2
+#    >>> altura(Nodo(Nodo(Hoja(1), Hoja(6)), Nodo(Hoja(2), Hoja(7))))
+#    2
+# ---------------------------------------------------------------------
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Arbol:
+    pass
+
+@dataclass
+class Hoja(Arbol):
+    x: int
+
+@dataclass
+class Nodo(Arbol):
+    i: Arbol
+    d: Arbol
+
+def altura(a: Arbol) -> int:
+    match a:
+        case Hoja(_):
+            return 0
+        case Nodo(i, d):
+            return 1 + max(altura(i), altura(d))
+    assert False
---
+++ tests/test_altura_de_un_arbol_binario.py
@@ -0,0 +1,11 @@
+from src.altura_de_un_arbol_binario import altura, Nodo, Hoja
+
+def test_altura() -> None:
+    assert altura(Hoja(1)) == \
+        0
+    assert altura(Nodo(Hoja(1), Hoja(6))) == \
+        1
+    assert altura(Nodo(Nodo(Hoja(1), Hoja(6)), Hoja(2))) == \
+        2
+    assert altura(Nodo(Nodo(Hoja(1), Hoja(6)), Nodo(Hoja(2), Hoja(7)))) == \
+        2
