00b43a47d6702a59ca82d81006102b4e06b250ae
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-12-15 13:02:23 +0100

    Profundidad de un árbol binario.

---
+++ src/profundidad_de_un_arbol_binario.py
@@ -0,0 +1,112 @@
+# profundidad_de_un_arbol_binario.py
+# Profundidad de un árbol binario.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 16-diciembre-2022
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# El árbol binario
+#         9
+#        / \
+#       /   \
+#      3     7
+#     / \
+#    2   4
+# se puede representar por
+#    N 9 (N 3 (H 2) (H 4)) (H 7)
+#
+# El tipo de los árboles binarios se puede definir por
+#    @dataclass
+#    class Arbol(Generic[A]):
+#        pass
+#
+#    @dataclass
+#    class H(Arbol[A]):
+#        x: A
+#
+#    @dataclass
+#    class N(Arbol[A]):
+#        x: A
+#        i: Arbol[A]
+#        d: Arbol[A]
+#
+# Definir la función
+#    profundidad : (Arbol[A]) -> int
+# tal que profundidad(x) es la profundidad del árbol x. Por ejemplo,
+#    profundidad(N(9, N(3, H(2), H(4)), H(7)))              ==  2
+#    profundidad(N(9, N(3, H(2), N(1, H(4), H(5))), H(7)))  ==  3
+#    profundidad(N(4, N(5, H(4), H(2)), N(3, H(7), H(4))))  ==  2
+#
+# Comprobar con Hypothesis que para todo árbol biario x, se tiene que
+#    nNodos(x) <= 2^profundidad(x) - 1
+# ---------------------------------------------------------------------
+
+from dataclasses import dataclass
+from random import choice, randint
+from typing import Generic, TypeVar
+
+from hypothesis import given
+from hypothesis import strategies as st
+
+A = TypeVar("A")
+
+@dataclass
+class Arbol(Generic[A]):
+    pass
+
+@dataclass
+class H(Arbol[A]):
+    x: A
+
+@dataclass
+class N(Arbol[A]):
+    x: A
+    i: Arbol[A]
+    d: Arbol[A]
+
+def profundidad(a: Arbol[A]) -> int:
+    match a:
+        case H(_):
+            return 0
+        case N(_, i, d):
+            return 1 + max(profundidad(i), profundidad(d))
+    assert False
+
+# Comprobación de equivalencia
+# ============================
+
+# (arbolArbitrario n) es un árbol aleatorio de orden n. Por ejemplo,
+#    >>> arbolArbitrario(4)
+#    N(x=2, i=H(x=1), d=H(x=9))
+#    >>> arbolArbitrario(4)
+#    H(x=10)
+#    >>> arbolArbitrario(4)
+#    N(x=4, i=N(x=7, i=H(x=4), d=H(x=0)), d=H(x=6))
+def arbolArbitrario(n: int) -> Arbol[int]:
+    if n <= 1:
+        return H(randint(0, 10))
+    m = n // 2
+    return choice([H(randint(0, 10)),
+                   N(randint(0, 10),
+                     arbolArbitrario(m),
+                     arbolArbitrario(m))])
+
+# nNodos(x) es el número de nodos del árbol x. Por ejemplo,
+#    nNodos(N(9, N(3, H(2), H(4)), H(7)))  ==  2
+def nNodos(a: Arbol[A]) -> int:
+    match a:
+        case H(_):
+            return 0
+        case N(_, i, d):
+            return 1 + nNodos(i) + nNodos(d)
+    assert False
+
+# La propiedad es
+@given(st.integers(min_value=1, max_value=10))
+def test_nHojas(n: int) -> None:
+    a = arbolArbitrario(n)
+    assert nNodos(a) <= 2 ** profundidad(a) - 1
+
+# La comprobación es
+#    src> poetry run pytest -q profundidad_de_un_arbol_binario.py
+#    1 passed in 0.11s
---
+++ tests/test_profundidad_de_un_arbol_binario.py
@@ -0,0 +1,7 @@
+from src.profundidad_de_un_arbol_binario import \
+    H, N, profundidad
+
+def test_profundidad() -> None:
+    assert profundidad(N(9, N(3, H(2), H(4)), H(7))) == 2
+    assert profundidad(N(9, N(3, H(2), N(1, H(4), H(5))), H(7))) == 3
+    assert profundidad(N(4, N(5, H(4), H(2)), N(3, H(7), H(4)))) == 2
