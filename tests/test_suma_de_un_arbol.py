2d4e70ad7d020ac51bb579c2abc244d88ac3f3fa
Jose A. Alonso <JoseA.Alonso@gmail.com>
2022-12-21 19:23:34 +0100

    Suma de un árbol.

---
+++ tests/test_suma_de_un_arbol.py
@@ -0,0 +1,8 @@
+from src.suma_de_un_arbol import \
+    H, N, sumaArbol
+
+def test_sumaArbol() -> None:
+    assert sumaArbol(N(2,
+                       N(5, N(3, H(), H()), N(7, H(), H())),
+                       N(4, H(), H()))) == \
+        21
