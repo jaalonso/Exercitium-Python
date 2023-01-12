eef79468e05b28fbfea50a1563a35b58c945fe2d
Jose A. Alonso <JoseA.Alonso@gmail.com>
2023-01-12 18:07:59 +0100

    Reconocimiento de subpilas.

---
+++ src/subPila.py
@@ -0,0 +1,128 @@
+# subPila.py
+# Reconocimiento de subpilas.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 31-enero-2023
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Utilizando el [tipo de dato de las pilas](https://bit.ly/3GTToyK)
+# (cuyo código se encuentra en [PilaConListas.hs](https://bit.ly/3vL41xD))
+# definir la función
+#    subPila : (Pila[A], Pila[A]) -> bool
+# tal que subPila(p1, p2) se verifica si p1 es una subpila de p2. Por
+# ejemplo,
+#    >>> ej1 = apila(2, apila(3, vacia()))
+#    >>> ej2 = apila(7, apila(2, apila(3, apila(5, vacia()))))
+#    >>> ej3 = apila(2, apila(7, apila(3, apila(5, vacia()))))
+#    >>> subPila(ej1, ej2)
+#    True
+#    >>> subPila(ej1, ej3)
+#    False
+# ---------------------------------------------------------------------
+
+from copy import deepcopy
+from typing import TypeVar
+
+from hypothesis import given
+
+from src.TAD.pilaConListas import (Pila, apila, cima, desapila, esVacia,
+                                   pilaAleatoria, vacia)
+
+A = TypeVar('A')
+
+# 1ª solución
+# ===========
+
+# prefijoPila(p1, p2) se verifica si la pila p1 es justamente un prefijo
+# de la pila p2. Por ejemplo,
+#    >>> ej1 = apila(4, apila(2, vacia()))
+#    >>> ej2 = apila(4, apila(2, apila(5, vacia())))
+#    >>> ej3 = apila(5, apila(4, apila(2, vacia())))
+#    >>> prefijoPila(ej1, ej2)
+#    True
+#    >>> prefijoPila(ej1, ej3)
+#    False
+def prefijoPila(p1: Pila[A], p2: Pila[A]) -> bool:
+    if esVacia(p1):
+        return True
+    if esVacia(p2):
+        return False
+    cp1 = cima(p1)
+    dp1 = desapila(p1)
+    cp2 = cima(p2)
+    dp2 = desapila(p2)
+    return cp1 == cp2 and prefijoPila(dp1, dp2)
+
+def subPila1(p1: Pila[A], p2: Pila[A]) -> bool:
+    if esVacia(p1):
+        return True
+    if esVacia(p2):
+        return False
+    cp1 = cima(p1)
+    dp1 = desapila(p1)
+    cp2 = cima(p2)
+    dp2 = desapila(p2)
+    if cp1 == cp2:
+        return prefijoPila(dp1, dp2) or subPila1(p1, dp2)
+    return subPila1(p1, dp2)
+
+# 2ª solución
+# ===========
+
+# pilaAlista(p) es la lista formada por los elementos de la
+# lista p. Por ejemplo,
+#    >>> pilaAlista(apila(5, apila(2, apila(3, vacia()))))
+#    [3, 2, 5]
+def pilaAlista(p: Pila[A]) -> list[A]:
+    if esVacia(p):
+        return []
+    cp = cima(p)
+    dp = desapila(p)
+    return pilaAlista(dp) + [cp]
+
+# sublista(xs, ys) se verifica si xs es una sublista de ys. Por
+# ejemplo,
+#    >>> sublista([3,2], [5,3,2,7])
+#    True
+#    >>> sublista([3,2], [5,3,7,2])
+#    False
+def sublista(xs: list[A], ys: list[A]) -> bool:
+    return any(xs == ys[i:i+len(xs)] for i in range(len(ys) - len(xs) + 1))
+
+def subPila2(p1: Pila[A], p2: Pila[A]) -> bool:
+    return sublista(pilaAlista(p1), pilaAlista(p2))
+
+# 3ª solución
+# ===========
+
+def subPila3Aux(p1: Pila[A], p2: Pila[A]) -> bool:
+    if p1.esVacia():
+        return True
+    if p2.esVacia():
+        return False
+    if p1.cima() != p2.cima():
+        p2.desapila()
+        return subPila3Aux(p1, p2)
+    q1 = deepcopy(p1)
+    p1.desapila()
+    p2.desapila()
+    return prefijoPila(p1, p2) or subPila3Aux(q1, p2)
+
+def subPila3(p1: Pila[A], p2: Pila[A]) -> bool:
+    q1 = deepcopy(p1)
+    q2 = deepcopy(p2)
+    return subPila3Aux(q1, q2)
+
+# Comprobación de equivalencia de las definiciones
+# ================================================
+
+# La propiedad es
+@given(p1=pilaAleatoria(), p2=pilaAleatoria())
+def test_subPila(p1: Pila[int], p2: Pila[int]) -> None:
+    r = subPila1(p1, p2)
+    assert subPila2(p1, p2) == r
+    assert subPila3(p1, p2) == r
+
+# La comprobación es
+#    src> poetry run pytest -q subPila.py
+#    1 passed in 0.32s
---
+++ tests/test_subPila.py
@@ -0,0 +1,10 @@
+from src.subPila import (apila, subPila1, subPila2, subPila3,
+                         vacia)
+
+def test_subPila() -> None:
+    ej1 = apila(2, apila(3, vacia()))
+    ej2 = apila(7, apila(2, apila(3, apila(5, vacia()))))
+    ej3 = apila(2, apila(7, apila(3, apila(5, vacia()))))
+    for subPila in [subPila1, subPila2, subPila3]:
+        assert subPila(ej1, ej2)
+        assert not subPila(ej1, ej3)
