886b3cb1ba217aa0af6c602808173a4a30121872
Jose A. Alonso <JoseA.Alonso@gmail.com>
2023-04-18 17:46:20 +0200

    TAD de los polinomios: Término líder de un polinomio.

---
+++ src/Pol_Termino_lider.py
@@ -0,0 +1,58 @@
+# Pol_Termino_lider.py
+# TAD de los polinomios: Término líder de un polinomio.
+# José A. Alonso Jiménez <https://jaalonso.github.io>
+# Sevilla, 25-abril-2023
+# ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+# Usando el [tipo abstracto de los polinomios](https://bit.ly/3KwqXYu),
+# definir la función
+#    termLider : (Polinomio[A]) -> Polinomio[A]
+# tal que termLider(p) es el término líder del polinomio p. Por
+# ejemplo,
+#    >>> ejPol = consPol(5, 1, consPol(2, 5, consPol(1, 4, polCero())))
+#    >>> ejPol
+#    x^5 + 5*x^2 + 4*x
+#    >>> termLider(ejPol)
+#    x^5
+# ---------------------------------------------------------------------
+
+# pylint: disable=unused-import
+
+from typing import TypeVar
+
+from hypothesis import given
+
+from src.TAD.Polinomio import (Polinomio, coefLider, grado, polCero,
+                               consPol, polinomioAleatorio)
+from src.Pol_Crea_termino import (creaTermino)
+
+A = TypeVar('A', int, float, complex)
+
+# 1ª solución
+# ===========
+
+def termLider(p: Polinomio[A]) -> Polinomio[A]:
+    return creaTermino(grado(p), coefLider(p))
+
+# 2ª solución
+# ===========
+
+def termLider2(p: Polinomio[A]) -> Polinomio[A]:
+    return creaTermino(p.grado(), p.coefLider())
+
+# La función creaTermino está definida en el ejercicio
+# "Construcción de términos" que se encuentra en
+# https://bit.ly/3GXteuH
+
+# Equivalencia de las definiciones
+# ================================
+
+# La propiedad es
+@given(p=polinomioAleatorio())
+def test_termLider(p: Polinomio[int]) -> None:
+    assert termLider(p) == termLider2(p)
+
+# La comprobación es
+#    > poetry run pytest -q Pol_Crea_termino.py
+#    1 passed in 0.21s
---
+++ tests/test_Pol_Termino_lider.py
@@ -0,0 +1,7 @@
+from src.Pol_Termino_lider import (termLider, termLider2)
+from src.TAD.Polinomio import (consPol, polCero)
+
+def test_termLider() -> None:
+    ejPol = consPol(5, 1, consPol(2, 5, consPol(1, 4, polCero())))
+    assert str(termLider(ejPol)) == "x^5"
+    assert str(termLider2(ejPol)) == "x^5"
